339f7ca901cca133339823cdd27edd2435c34d9f
yanghaibo <452972031@qq.com>
2022-04-10 03:08:44 +0800

    update

--- src/api/config.js
+++ src/api/config.js
@@ -2,7 +2,7 @@ let BASE_URL = '/api'
 if (process.env.NODE_ENV == 'development') {
   BASE_URL = 'http://localhost:5000/'
 } else if(process.env.NODE_ENV == 'production'){
-  BASE_URL = 'http://wx.unechannel.com:5000/'
+  BASE_URL = 'http://unechannel.com:8080:5000/'
 }
 
 export { BASE_URL }
