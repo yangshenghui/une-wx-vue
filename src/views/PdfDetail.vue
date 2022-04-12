14c579a1fb7d7cde0772b9ec58819daba31a276d
yanghaibo <452972031@qq.com>
2022-04-12 18:31:04 +0800

    update

--- src/views/PdfDetail.vue
+++ src/views/PdfDetail.vue
@@ -7,37 +7,26 @@
       <div class="une-loading-table" v-if="loading">
         <van-loading color="#1989fa" class="une-loading-cell"/>
       </div>
-      <pdf
-        ref="pdf"
-        v-for="i in numPages"
-        :key="i"
-        :src="url"
-        :page="i"
-        @page-loaded="pageLoaded($event)"
-      ></pdf>
+      <div id="demo"></div>
     </div>
   </div>
 </template>
 <script>
-    import pdf from 'vue-pdf'
-
+    import Pdfh5 from "pdfh5";
+    import "pdfh5/css/pdfh5.css";
     export default {
-        components: {
-            pdf
-        },
+        name: 'pdfH5',
         data() {
             return {
+                pdfh5: null,
                 loading: true,
+                vedioId: "",
                 url: "",
                 vip: "",
-                numPages: null, // pdf 总页数
-                read: {},
-                vedioId: "",
                 ismember: "",
-            }
+            };
         },
         created () {
-            console.log("dadada")
             if(this.$route.params && this.$route.params.pdfUrl) {
                 console.log(this.$route.params)
                 this.vedioId = this.$route.params.vedioId;
@@ -50,31 +39,16 @@
 
         },
         mounted() {
-            this.getNumPages()
-        },
-        methods: {
-            pageLoaded(pageLoaded) {
-              if(pageLoaded == this.numPages)  {
-                  this.loading = false;
-              }
-            },
-            onClickLeft() {
-                if(this.vedioId != undefined) {
-                    this.$router.push({ name: 'Vedio', params: { vedioId: this.vedioId }});
-                } else {
-                    this.$router.go(-1)
-                }
-
-            },
-            getNumPages() {
+            //实例化
+            this.pdfh5 = new Pdfh5("#demo", {
+                pdfurl: "http://cdn.unechannel.com/SpringBoot%E5%90%AF%E5%8A%A8%E6%B5%81%E7%A8%8B%E8%A7%A3%E6%9E%90.pdf"
+            });
+            //监听完成事件
+            this.pdfh5.on("complete", (status, msg, time) => {
+                console.log("状态：" + status + "，信息：" + msg + "，耗时：" + time + "毫秒，总页数：" + this.totalNum)
+                this.loading = false;
                 if(this.vip == "2" && this.ismember == 0) {
-                    console.log(this.url)
-                    let loadingTask = pdf.createLoadingTask(this.url)
-                    loadingTask.promise.then(pdf => {
-                        this.numPages = 2
-                    }).catch(err => {
-                        console.error('pdf 加载失败', err);
-                    })
+                    this.pdfh5.scrollEnable(false)
                     this.$dialog.confirm({
                         title: '开通会员',
                         message: '开通会员看完整文档',
@@ -83,13 +57,16 @@
                         this.$router.push({ name: 'Member'});
                     }).catch(() => {
                     });
+                }
+
+            })
+        },
+        methods:{
+            onClickLeft() {
+                if(this.vedioId != undefined) {
+                    this.$router.push({ name: 'Vedio', params: { vedioId: this.vedioId }});
                 } else {
-                    let loadingTask = pdf.createLoadingTask(this.url)
-                    loadingTask.promise.then(pdf => {
-                        this.numPages = pdf.numPages
-                    }).catch(err => {
-                        this.$notify({ type: 'error', message: 'pdf 加载失败' });
-                    })
+                    this.$router.go(-1)
                 }
 
             },
@@ -115,5 +92,40 @@
     display: table-cell;
     vertical-align: middle;
     text-align: center;
+  }
+
+  *{
+    padding: 0;
+    margin: 0;
+  }
+  html,body {
+    width: 100%;
+    height: 100%;
+  }
+  .pdf{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: #000;
+    overflow: hidden;
+    z-index: 200;
+  }
+  .iconContain{
+    position: fixed;
+    top: .426667rem;
+    right: .426667rem;
+    width: .64rem;
+    height: .64rem;
+    line-height: .64rem;
+    text-align: center;
+    background: rgba(0, 0, 0, 0.4);
+    z-index: 200;
+    border-radius: 50%;
+  }
+  .iconClose{
+    display: block;
+    color:#fff;
   }
 </style>
